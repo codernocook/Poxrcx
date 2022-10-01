5e4e3dc175d448864776db9f9bb64011263c8fa1
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-01 16:59:53 +0700

    updated real bot ping

--- commandmodule/ping.js
+++ commandmodule/ping.js
@@ -2,7 +2,10 @@ module.exports = {
     name: 'Ping',
     description: "Reply with pong!",
     execute(argument, message, EmbedBuilder) {
-        message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Pong with 0 ping!`).setColor(`Green`)] })
+        message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Bot ping is: 0`).setColor(`Green`)] }).then(messageget => {
+            const ping = messageget.createdTimestamp - message.createdTimestamp;
+            messageget.edit({ embeds: [new EmbedBuilder().setDescription(`Bot ping is: ${ping}`).setColor(`Green`)] })
+        })
         //message.channel.send(`Pong with 0 ping!`)
     }
 }
