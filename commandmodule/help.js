c6c0e5ad83da7ab8f8aa2f284d8437ce7f46c3c7
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-04 12:42:11 +0700

    Update help.js

--- commandmodule/help.js
+++ commandmodule/help.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'Help',
     description: "Show the help info!",
-    execute(argument, message, EmbedBuilder, messagetype) {
-        message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Prefix: ./\nping: Reply with pong.\nKick: [user] [reason]\nBan: [user] [reason]\nUnban: [user] [reason]\nTimeout [user] [length] [reason]\nKill: [user] (kill user you want but it useless, just wait for another update)\nAnnoy: Don't try to annoy me, it will make you a :clown:, :smiling_imp:!\nRat: it can hack to anyone [computer/phone] (its a fun command don't worry it not real)\nMeme: send a random meme in chat!\nHelp page:https://pox-commandx-website.vercel.app/help\nVersion: 2.0.0\nMade by Itzporium!`).setColor(`Blue`)] })
+    execute(argument, message, EmbedBuilder) {
+        message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Prefix: ./\nPing: Reply with pong.\nKick: [user] [reason]\nBan: [user] [reason]\nUnban: [user] [reason]\nTimeout [user] [length] [reason]\nKill: [user]\nAnnoy: Don't try to annoy me, it will make you a :clown:, :smiling_imp:!\nRat: it can hack to anyone [computer/phone] (its a fun command don't worry it not real)\nMeme: send a random meme in chat!\nHelp page: https://pox-commandx-website.vercel.app/help\nVersion: 2.0.0\nMade by Itzporium!`).setColor(`Blue`)] })
     }
 }
