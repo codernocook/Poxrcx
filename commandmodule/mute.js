3080780a3843a7078383b6184ec35440252838e3
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-05 12:24:05 +0700

    Update mute.js

--- commandmodule/mute.js
+++ commandmodule/mute.js
@@ -6,13 +6,14 @@ module.exports = {
     execute(argument, message, EmbedBuilder) {
         if (!argument[0]) return  message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild user.`).setColor(`Red`)] })
         if (!argument[1]) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Missing mute duration!`).setColor(`Red`)] })
-            const mentioneduser = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0])
+            const mentioneduser = message.mentions.members.first();
             const parsetime = require('parse-duration').default;
             const mspack = require('ms');
             if (!mentioneduser) return message.channel.send("Invaild user.")
             if (!message.member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to mute user!`).setColor(`Red`)] })
             //if (!message.guild) return message.channel.send("I don't have permission to time out people please enable it!")
             if (message.member === mentioneduser) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You can't mute yourself.`).setColor(`Red`)] })
+            if (mentioneduser.roles.highest.position > message.author.roles.highest.position) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to use this command!`).setColor(`Red`)] });
 
             const parsedtime = parsetime(argument[1])
             let reason = argument.slice(1).join(" ") || 'No reason given'
