8e2d3a0f675cc188f138c6a37b25fe486a460892
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-05 12:28:07 +0700

    Update kick.js

--- commandmodule/kick.js
+++ commandmodule/kick.js
@@ -2,15 +2,16 @@ module.exports = {
     name: 'Kick',
     description: "Kick someone from the server!",
     execute(argument, message, EmbedBuilder) {
-        const member = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0])
+        const member = message.mentions.members.first();
         if (!member) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild user!`).setColor(`Red`)] })
         if (!argument[1]) argument[1] = "No reason given."
         if (!message.member.permissions.has("KickMembers") && !message.member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to kick!`).setColor(`Red`)] })
         if (member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to kick this user!`).setColor(`Red`)] })
+        if (member.roles.highest.position > message.author.roles.highest.position) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to use this command!`).setColor(`Red`)] });
         if (member) {
             const membertarget = message.guild.members.cache.get(member.id);
             membertarget.kick({ reason: `${argument[1]}` });
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`:white_check_mark: Kicked ${member} for ${argument[1]}`).setColor(`Green`)] })
+            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> Kicked ${member} for ${argument[1]}`).setColor(`Green`)] })
         }else{
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild user!`).setColor(`Red`)] })
         }
