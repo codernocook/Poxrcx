a38c75a6de4f229abf0b83ef6e75eefa7f915c5c
codernocook <90613458+codernocook@users.noreply.github.com>
2022-10-05 12:34:30 +0700

    Update unban.js

--- commandmodule/unban.js
+++ commandmodule/unban.js
@@ -9,7 +9,6 @@ module.exports = {
             if (!message.member.permissions.has("BanMembers") && !message.member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to use this command!`).setColor(`Red`)] });
             //if (!message) return message.channel.send("I don't have permission to ban people please enable it!")
             if (message.member.user.id === mentioneduser) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You can't unban yourself!`).setColor(`Red`)] })
-            if (mentioneduser.roles.highest.position > message.author.roles.highest.position) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to use this command!`).setColor(`Red`)] });
 
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> Unbanned user ${ "<@" + mentioneduser + ">"}.`).setColor(`Green`)] })
             message.guild.members.unban(mentioneduser, argument[1]).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't unban this user`).setColor(`Red`)] })})
