00896066d76b223ccd674aacd279ad12ddda2021
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-14 14:35:14 +0000

    New Fix Correction and dumb reason checker

--- commandmodule/mute.js
+++ commandmodule/mute.js
@@ -14,12 +14,18 @@ module.exports = {
             //if (!message.guild) return message.channel.send("I don't have permission to time out people please enable it!")
             if (message.member === mentioneduser) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You can't mute yourself.`).setColor(`Red`)] })
 
+             //Check position to not abuse or exploit
+            const mentioneduserposition = mentioneduser.roles.highest.position
+            const authorsendposition = message.author.roles.highest.position
+
+            if (!mentioneduserposition > authorsendposition) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> That user is a moderator, I can't do that.`).setColor(`Red`)] })
+
             const parsedtime = parsetime(argument[1])
-            let reason = argument.slice(1).join(" ") || 'No reason given'
+            let reason = argument.slice(2).join(" ") || 'No reason given.'
 
             if (parsedtime < mspack("1m") || parsedtime > mspack("14d")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Please use vaild limit less than 14 days and more than 1 minute.`).setColor(`Red`)] });
 
-            mentioneduser.timeout(parsedtime, reason).catch(err => { message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't mute ${mentioneduser}`).setColor(`Red`)] }) });
+            mentioneduser.timeout(parsedtime, reason).catch(err => { message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't mute ${mentioneduser}, maybe my role position is too low.`).setColor(`Red`)] }) });
 
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> User ${mentioneduser} was muted.`).setColor(`Green`)] })
     }
