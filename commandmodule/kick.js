6359d22c1cea4606cc53251a054de4f2ea64dfb2
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-14 14:29:53 +0000

    Added Position Check for Pox

--- commandmodule/kick.js
+++ commandmodule/kick.js
@@ -2,16 +2,23 @@ module.exports = {
     name: 'Kick',
     description: "Kick someone from the server!",
     execute(argument, message, EmbedBuilder) {
-        const member = message.mentions.members.first();
+        const member = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0]);
         if (!member) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild user!`).setColor(`Red`)] })
-        if (!argument[1]) argument[1] = "No reason given."
         if (!message.member.permissions.has("KickMembers") && !message.member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to kick!`).setColor(`Red`)] })
         if (member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to kick this user!`).setColor(`Red`)] })
 
+        //Check position to not abuse or exploit
+        const mentioneduserposition = member.roles.highest.position
+        const authorsendposition = message.author.roles.highest.position
+
+        if (!mentioneduserposition > authorsendposition) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> That user is a moderator, I can't do that.`).setColor(`Red`)] })
+
+        let reason = argument.slice(2).join(" ") || 'No reason given.'
+
         if (member) {
             const membertarget = message.guild.members.cache.get(member.id);
-            membertarget.kick({ reason: `${argument[1]}` });
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> Kicked ${member} for ${argument[1]}`).setColor(`Green`)] })
+            membertarget.kick({ reason: `${reason}` });
+            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> Kicked ${member} for ${reason}`).setColor(`Green`)] }).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't kick this user, maybe my discord bot position is too low.`).setColor(`Red`)] })})
         }else{
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild user!`).setColor(`Red`)] })
         }
