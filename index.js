00ba0c20450135cbf4200fac74ffc7279e560484
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-14 14:53:36 +0000

    New Command Delay function
    This will help to not crash the bot.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 const token = process.env.token; //"MTAyMzU4NTU5MjE1MzQ3MzEyNQ.G9ooDD.lC8CxSEA3qEArrtJeqakoAcKA_R4HH6ptyZppE";
 const rest = new REST({ version: '10' }).setToken(token);
 const prefix = process.env.prefix; //"./";
+let commandcooldown = new Set();
+let cooldownsleep = async (ms) => await new Promise(r => setTimeout(r,ms));
 
 function executefile(filerequire, argumentsend, messagesend) {
     if (require(`./commandmodule/${filerequire}`)) {
@@ -15,53 +17,60 @@ client.on("ready", () => {
     console.log("Running Discord bot")
 })
 
-client.on("messageCreate", (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("messageCreate", async (message) => {
+    if(commandcooldown.has(message.author.id)) {
+        message.channel.send("Wah slow down you are too fast!");
+    } else {
+        if (!message.content.startsWith(prefix) || message.author.bot) return; // check if dumb discord send message.
+        commandcooldown.add(message.author.id);
+        //Run the command checker
+        await cooldownsleep(100)
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+        const messaggearray = message.content.split(" ");
+        const argument = messaggearray.slice(1);
+        const cmd = messaggearray[0];
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    const messaggearray = message.content.split(" ");
-    const argument = messaggearray.slice(1);
-    const cmd = messaggearray[0];
+        //command
 
-    //command
-
-    if (command === "help") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "ping") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "kick") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "ban") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "unban") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "mute") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "unmute") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "annoy") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "kill") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "rat") {
-        executefile(`${command}`, argument, message)
-    }
-    if (command === "meme") {
-        executefile(`${command}`, argument, message)
+        if (command === "help") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "ping") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "kick") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "ban") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "unban") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "mute") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "unmute") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "annoy") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "kill") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "rat") {
+            executefile(`${command}`, argument, message)
+        }
+        if (command === "meme") {
+            executefile(`${command}`, argument, message)
+        }
+        commandcooldown.delete(message.author.id);
     }
 })
 
-client.on('interactionCreate', (interaction) => {
+client.on('interactionCreate', async (interaction) => {
     client.guilds.cache.get(interaction.guild.id).commands.create({name: "ping", description: "Reply with pong!"})
     client.guilds.cache.get(interaction.guild.id).commands.create({name: "help", description: "Show the help info!"})
     if (!interaction.isChatInputCommand()) return;
