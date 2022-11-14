78378a4dee1efea8be26b9f8e827d580db50a8a6
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-14 14:30:26 +0000

    Fix my correction

--- commandmodule/ban.js
+++ commandmodule/ban.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Ban someone from the server!",
     execute(argument, message, EmbedBuilder) {
         if (!argument[0]) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild User!`).setColor(`Red`)] })
-            const mentioneduser = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0])
+            const mentioneduser = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0]);
             if (!mentioneduser) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Invaild User!`).setColor(`Red`)] })
             if (!message.member.permissions.has("BanMembers") && !message.member.permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to ban!`).setColor(`Red`)] })
             //if (!message) return message.channel.send("I don't have permission to ban people please enable it!")
@@ -17,7 +17,7 @@ module.exports = {
             if (!mentioneduserposition > authorsendposition) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> That user is a moderator, I can't do that.`).setColor(`Red`)] })
 
             // Checking if reason value is filled or not
-            let reason = argument.slice(1).join(" ") || 'No reason given'
+            let reason = argument.slice(1).join(" ") || 'No reason given.'
             // Start banning
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> Banned user ${mentioneduser} for ${reason}.`).setColor(`Green`)] })
             mentioneduser.ban({ reason: `${argument[1]}` }).catch(err => {message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> I can't ban this user, maybe my discord bot position is too low.`).setColor(`Red`)] })})
