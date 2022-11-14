d101106dda3a68a36c79893f2493e4f78e7fd53d
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-14 14:37:41 +0000

    New Fix to no crash

--- commandmodule/annoy.js
+++ commandmodule/annoy.js
@@ -1,15 +1,9 @@
 module.exports = {
     name: 'annoy',
     description: "annoy the bot",
-    execute(argument, message, EmbedBuilder, messagetype) {
-        if (messagetype === "interaction") {
-            message.reply("STOP ANNOYING BITCH")
-            message.reply(`everyone THIS SHIT IS ${message.author} ANNOYING ME!`)
-            message.reply({ embeds: [new EmbedBuilder().setDescription(`${message.author} got banned forever.`).setColor(`Green`)] })
-        }else {
-            message.channel.send("STOP ANNOYING BITCH")
-            message.channel.send(`everyone THIS SHIT IS ${message.author} ANNOYING ME!`)
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${message.author} got banned forever.`).setColor(`Green`)] })
-        }
+    execute(argument, message, EmbedBuilder) {
+        message.channel.send("STOP ANNOYING BITCH")
+        message.channel.send(`everyone THIS SHIT IS ${message.author} ANNOYING ME!`)
+        message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${message.author} got banned forever.`).setColor(`Green`)] })
     }
 }
