44a4d90559346fe3a89fb285dcb9f8f4b9d2c335
codernocook <90613458+codernocook@users.noreply.github.com>
2022-11-18 23:48:09 +0700

    I fixed the code on mobile, master

--- commandmodule/rat.js
+++ commandmodule/rat.js
@@ -8,17 +8,17 @@ module.exports = {
             option.setName("user").setDescription("User to hack").setRequired(true)
         ),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
-        const member = message.mentions.members.first();
-        const memberinteration = message.options.getMentionable("user")
         if (typeofcommand === "message") {
+          const member = message.mentions.members.first();
             if (member) {
                 message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> User ${member} got hacked, i also installed a rat in their computer, i sent ${member} information to everyone`).setColor(`Green`)] })
             }else {
                 message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Failed to hack this user!`).setColor(`Red`)] })
             }
         } else if (typeofcommand === "interaction"){
-            if (member) {
-                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> User ${memberinteration} got hacked, i also installed a rat in their computer, i sent ${memberinteration} information to everyone`).setColor(`Green`)] })
+          const memberinteraction = message.options.getMentionable("user");
+            if (memberinteraction) {
+                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> User ${memberinteraction} got hacked, i also installed a rat in their computer, i sent ${memberinteraction} information to everyone`).setColor(`Green`)] })
             }else {
                 message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Failed to hack this user!`).setColor(`Red`)] })
             }
