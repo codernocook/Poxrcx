bbe50a9572e239c008846d7123f30697d852cf16
codernocook <nhatlong.vn69@gmail.com>
2022-11-18 16:14:58 +0000

    Remove Too much help infomation and replace with website domain

--- commandmodule/help.js
+++ commandmodule/help.js
@@ -6,9 +6,9 @@ module.exports = {
 		.setDescription("Show the help info!"),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
         if (typeofcommand === "message") {
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Prefix: ./\nPing: Reply with pong.\nKick: [user] [reason]\nBan: [user] [reason]\nUnban: [user] [reason]\nMute [user] [length] [reason]\nUnmute [user] [reason]\nKill: [user]\nAnnoy: Don't try to annoy me, it will make you a :clown:, :smiling_imp:!\nRat: it can hack to anyone [computer/phone] (its a fun command don't worry it not real)\nMeme: send a random meme in chat!\nHelp page: https://pox-commandx-website.vercel.app/help\nVersion: 2.0.5\nMade by Itzporium!`).setColor(`Blue`)] })
+            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Help page: https://poxrcx.vercel.app/help/\nSupport Server: https://poxrcx.vercel.app/invite/\nVersion: 2.0.5\nMade by Itzporium!`).setColor(`Blue`)] })
         } else if (typeofcommand === "interaction"){
-            message.reply({ embeds: [new EmbedBuilder().setDescription(`Prefix: ./\nPing: Reply with pong.\nKick: [user] [reason]\nBan: [user] [reason]\nUnban: [user] [reason]\nMute [user] [length] [reason]\nUnmute [user] [reason]\nKill: [user]\nAnnoy: Don't try to annoy me, it will make you a :clown:, :smiling_imp:!\nRat: it can hack to anyone [computer/phone] (its a fun command don't worry it not real)\nMeme: send a random meme in chat!\nHelp page: https://pox-commandx-website.vercel.app/help\nVersion: 2.0.5\nMade by Itzporium!`).setColor(`Blue`)] })
+            message.reply({ embeds: [new EmbedBuilder().setDescription(`Help page: https://poxrcx.vercel.app/help/\nSupport Server: https://poxrcx.vercel.app/invite/\nVersion: 2.0.5\nMade by Itzporium!`).setColor(`Blue`)] })
         }
     }
 }
--- commandmodule/stats.js
+++ commandmodule/stats.js
@@ -8,7 +8,7 @@ module.exports = {
         if (typeofcommand === "message") {
             message.channel.send({ embeds: [new EmbedBuilder().setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMembers: ${message.guild.memberCount}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] })
         } else if (typeofcommand === "interaction"){
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMembers: ${message.guild.memberCount}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] })
+            message.reply({ embeds: [new EmbedBuilder().setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMembers: ${message.guild.memberCount}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] })
         }
     }
 }
