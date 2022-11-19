81063d4961ae7a410bedd0309a2080e2d7255a3f
codernocook <nhatlong.vn69@gmail.com>
2022-11-19 02:36:47 +0000

    fixed?afk

--- commandmodule/afk.js
+++ commandmodule/afk.js
@@ -3,26 +3,29 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder()
 		.setName("afk")
-		.setDescription("Set your status to Afk"),
+		.setDescription("Set your status to Afk")
+        .addStringOption(option =>
+            option.setName("message").setDescription("the message you want other user know.").setRequired(false)
+        ),
     execute(argument, message, EmbedBuilder, client, typeofcommand, afk) {
         if (typeofcommand === "message") {
             let reason = argument.slice(2).join(" ");
             afk.set(toString(message.author.id), [ Date.now(), reason ])
             message.author.setNickname(`[AFK] ${message.author.username}`)
             if (reason) {
-                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> ${message.author}. You are now afk for ${reason}.`).setColor(`Green`)] })
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.author.id}> You are now afk for ${reason}.`).setColor(`Green`)] })
             } else {
-                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> ${message.author}. You are now afk.`).setColor(`Green`)] })
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.author.id}> You are now afk.`).setColor(`Green`)] })
             }
         } else if (typeofcommand === "interaction"){
-            let reason = argument.slice(2).join(" ");
+            let reason = message.options.getString("message")
             afk.set(toString(message.user.id), [ Date.now(), reason ])
             message.guild.members.cache.find(user => interaction.user.id).setNickname(`[AFK] ${interaction.user.username}`)
 
             if (reason) {
-                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}>. You are now afk for ${reason}.`).setColor(`Green`)] })
+                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}> You are now afk for ${reason}.`).setColor(`Green`)] })
             } else {
-                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}>. You are now afk.`).setColor(`Green`)] })
+                message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}> You are now afk.`).setColor(`Green`)] })
             }
         }
     }
