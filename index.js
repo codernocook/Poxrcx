9288cbfe0509fffbb6891162523564cabd0f7ea9
codernocook <nhatlong.vn69@gmail.com>
2022-11-20 09:33:48 +0000

    AFK Fix

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ client.on("messageCreate", async (message) => {
             if (afkset.has(mentionget.id)) {
                 if (Number(message.guildId) === Number(afkset.get(mentionget.id)[3].id)) {
                     const timeago = moment(afkset.get(mentionget.id)[1]).fromNow();
+                    message.channel.send(`\`${mentionget.user.username}\` afked for **${timeago}**, AFK Message: ${afkset.get(mentionget.id)[2]}.`)
 
                     if (!afkset.get(mentionget.id)[2] === undefined) {
                         message.channel.send(`\`${mentionget.user.username}\` afked for **${timeago}**, AFK Message: ${afkset.get(mentionget.id)[2]}.`)
