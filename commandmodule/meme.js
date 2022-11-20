de9dde5df9118595feeded50a35b61f772f5ca7a
codernocook <nhatlong.vn69@gmail.com>
2022-11-20 05:44:36 +0000

    New Meme Modules Update

--- commandmodule/meme.js
+++ commandmodule/meme.js
@@ -1,18 +1,25 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const RandomPuppy = require("random-puppy")
+const got = require("got");
 
 module.exports = {
     data: new SlashCommandBuilder()
 		.setName("meme")
 		.setDescription("Get a random meme!"),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
-        const SubReddit = ["memes", "dankmemes"]
-        const randomsearch = SubReddit[Math.floor(Math.random() * SubReddit.length)];
-        const memeimages = RandomPuppy(randomsearch);
-        if (typeofcommand === "message") {
-            message.channel.send({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`https://reddit.com/r/${random}`).setColor(`Blue`)] });
-        } else if (typeofcommand === "interaction"){
-            message.reply({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`https://reddit.com/r/${random}`).setColor(`Blue`)] });
-        }
+        got("https://reddit.com/r/memes/random/.json").then(response => {
+            let content = JSON.parse(response.body);
+            let permalink = content[0].data.children[0].data.permalink;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].data.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            if (typeofcommand === "message") {
+                message.channel.send({ embeds: [new EmbedBuilder().setTitle(`${memeTitle}`).setURL(`${memeUrl}`).setImage(memeImage).setFooter(`:thumbsup: ${memeUpvotes} | :thumbsdown: ${memeDownvotes} | :envelope: ${memeNumComments}`).setColor(`Blue`)] });
+            } else if (typeofcommand === "interaction"){
+                message.reply({ embeds: [new EmbedBuilder().setTitle(`${memeTitle}`).setURL(`${memeUrl}`).setImage(memeImage).setFooter(`:thumbsup: ${memeUpvotes} | :thumbsdown: ${memeDownvotes} | :envelope: ${memeNumComments}`).setColor(`Blue`)] });
+            }
+        })
     }
 }
