2e1cb08c9801f88fd3f1ff8890b6d4a60b3c737e
codernocook <nhatlong.vn69@gmail.com>
2022-11-20 05:19:45 +0000

    Fixed I thing

--- commandmodule/meme.js
+++ commandmodule/meme.js
@@ -6,13 +6,13 @@ module.exports = {
 		.setName("meme")
 		.setDescription("Get a random meme!"),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
-        const SubReddit = ["meme", "me_irl", "dankmeme"]
+        const SubReddit = ["memes", "dankmemes"]
         const randomsearch = SubReddit[Math.floor(Math.random() * SubReddit.length)];
         const memeimages = RandomPuppy(randomsearch);
         if (typeofcommand === "message") {
-            message.channel.send({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`http://reddit.com/${random}`).setColor(`Blue`)] });
+            message.channel.send({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`https://reddit.com/r/${random}`).setColor(`Blue`)] });
         } else if (typeofcommand === "interaction"){
-            message.reply({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`http://reddit.com/${random}`).setColor(`Blue`)] });
+            message.reply({ embeds: [new EmbedBuilder().setTitle(`From /r/%${randomsearch}`).setImage(memeimages).setURL(`https://reddit.com/r/${random}`).setColor(`Blue`)] });
         }
     }
 }
