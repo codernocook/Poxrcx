cb49647e25be021c29015a1a5d01b0095afeeb9f
codernocook <nhatlong.vn69@gmail.com>
2022-12-05 13:39:37 +0000

    Fix my bad

--- commandmodule/poll.js
+++ commandmodule/poll.js
@@ -12,14 +12,14 @@ module.exports = {
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)] })
             let pollmessage = argument.slice(0).join(" ")
             if (!pollmessage) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            let pollmessageembed = message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).set.setColor(`Blue`)] })
+            let pollmessageembed = message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).setColor(`Blue`)] })
             pollmessageembed.react(":thumbsup:")
             pollmessageembed.react(":thumbsdown:")
         } else if (typeofcommand === "interaction"){
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)], ephemeral: true  })
             let pollmessage = message.options.getString("choices")
             if (!pollmessage) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            let pollmessageembed = message.reply({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).set.setColor(`Blue`)] })
+            let pollmessageembed = message.reply({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).setColor(`Blue`)] })
             pollmessageembed.react(":thumbsup:")
             pollmessageembed.react(":thumbsdown:")
         }
