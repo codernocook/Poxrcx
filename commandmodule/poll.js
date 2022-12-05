24fdeb489efcef72406c2874e592cd1fa22e5ca4
codernocook <nhatlong.vn69@gmail.com>
2022-12-05 13:36:00 +0000

    Made better poll instead of bad poll npm packages

--- commandmodule/poll.js
+++ commandmodule/poll.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { poll } = require("discord.js-poll")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,13 +12,16 @@ module.exports = {
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)] })
             let pollmessage = argument.slice(0).join(" ")
             if (!pollmessage) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            poll(pollmessage, argument, "+", "#00D1CD")
+            let pollmessageembed = message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).set.setColor(`Blue`)] })
+            pollmessageembed.react(":thumbsup:")
+            pollmessageembed.react(":thumbsdown:")
         } else if (typeofcommand === "interaction"){
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)], ephemeral: true  })
             let pollmessage = message.options.getString("choices")
-            let args = pollmessage.split().slice(0)
             if (!pollmessage) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            poll(pollmessage, args, "+", "#00D1CD")
+            let pollmessageembed = message.reply({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).set.setColor(`Blue`)] })
+            pollmessageembed.react(":thumbsup:")
+            pollmessageembed.react(":thumbsdown:")
         }
     }
 }
