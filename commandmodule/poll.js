c32db1171abef36747f313d5beb5ea58c6a73dcf
codernocook <nhatlong.vn69@gmail.com>
2022-12-05 13:57:38 +0000

    Fixed poll for normal command calling

--- commandmodule/poll.js
+++ commandmodule/poll.js
@@ -3,10 +3,13 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("poll")
-		.setDescription("Start a poll only Upvote and Downvote."),
+		.setDescription("Start a poll only Upvote and Downvote.")
+        .addStringOption(option =>
+            option.setName("title").setDescription("The poll title of the poll").setRequired(true)
+        ),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
         if (typeofcommand === "message") {
-            if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)] })
+            if (!message.guild.members.cache.find(user => message.author.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.author.id === user.id).permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)] })
             let pollmessage = argument.slice(0).join(" ")
             if (!pollmessage) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
             let pollmessageembed = message.channel.send({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).setColor(`Blue`)] })
@@ -14,8 +17,7 @@ module.exports = {
             pollmessageembed.react(":thumbsdown:")
         } else if (typeofcommand === "interaction"){
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)], ephemeral: true  })
-            let pollmessage = message.options.getString("choices")
-            if (!pollmessage) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
+            let pollmessage = message.options.getString("title")
             let pollmessageembed = message.reply({ embeds: [new EmbedBuilder().setDescription(`${pollmessage}\n👍: Upvote | 👎: Downvote`).setColor(`Blue`)] })
             pollmessageembed.react(":thumbsup:")
             pollmessageembed.react(":thumbsdown:")
