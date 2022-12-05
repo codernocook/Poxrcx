edda799eb2d886844c04448071295110af343a0b
codernocook <nhatlong.vn69@gmail.com>
2022-12-05 13:20:39 +0000

    Fixed by change Poll => poll

--- commandmodule/poll.js
+++ commandmodule/poll.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { Poll } = require("discord.js-poll")
+const { poll } = require("discord.js-poll")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,13 +13,13 @@ module.exports = {
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)] })
             let pollmessage = argument.slice(0).join(" ")
             if (!pollmessage) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            Poll(pollmessage, argument, "+", "#00D1CD")
+            poll(pollmessage, argument, "+", "#00D1CD")
         } else if (typeofcommand === "interaction"){
             if (!message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("ManageEvents") && !message.guild.members.cache.find(user => message.user.id === user.id).permissions.has("Administrator")) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You don't have permission to start a poll.`).setColor(`Red`)], ephemeral: true  })
             let pollmessage = message.options.getString("choices")
             let args = pollmessage.split().slice(0)
             if (!pollmessage) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You need a choices to start a poll.`).setColor(`Red`)] })
-            Poll(pollmessage, args, "+", "#00D1CD")
+            poll(pollmessage, args, "+", "#00D1CD")
         }
     }
 }
