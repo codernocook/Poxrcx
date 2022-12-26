c07682fae4aa00da4faa99698d8e4f1a9834ef14
codernocook <90613458+codernocook@users.noreply.github.com>
2022-12-26 03:05:31 +0000

    20 commands with rps game!!
    We will make type of command like this, and more game and command to use. Hope you like the bot!

---
+++ commandmodule/rps.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const rpschar = ['Rock', 'Paper', 'Scissor']
+
+function rpsgame(reqsent, botsent) {
+    const req = reqsent.toLowerCase();
+    const bot = botsent.toLowerCase();
+    //check if req and bot is not missing
+    if (!req || !bot) return
+    // if the req and bot is match then we gonna return (match)
+    if (req === bot) return "Match!"
+
+    // Win
+    if ((req === "rock" && bot === "scissors") || (req === "paper" && bot === "rock" || (req === "scissor" && bot === "paper"))) {
+        return "You win!"
+    }
+    //Lose
+    if ((req === "rock" && bot === "paper") || (req === "paper" && bot === "scissors") || (req === "scissor" && bot === "rock")) {
+        return "You lose!"
+    }
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+		.setName("rps")
+		.setDescription("Play rock, paper, scissior.")
+        .addStringOption(option =>
+            option.setName("choice").setDescription("Your choice: Rock, Paper, Scissor.").setRequired(true)
+        ),
+    execute(argument, message, EmbedBuilder, client, typeofcommand) {
+        if (typeofcommand === "message") {
+            const botrpsanswer = rpschar[Math.floor(Math.random()*rpschar.length)]
+            const botrpsrandom = rpsgame(argument.join(" ").trim(), botrpsanswer)
+
+            if (botrpsrandom === "You win!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${argument.join(" ").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Green`)] })
+            } else if (botrpsrandom === "You lose!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${argument.join(" ").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Red`)] })
+            } else if (botrpsrandom === "Match!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${argument.join(" ").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Blue`)] })
+            }
+        } else if (typeofcommand === "interaction"){
+            const botrpsanswer = rpschar[Math.floor(Math.random()*rpschar.length)]
+            const botrpsrandom = rpsgame(message.options.getString("choice").trim(), botrpsanswer)
+
+            if (botrpsrandom === "You win!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${message.options.getString("choice").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Green`)] })
+            } else if (botrpsrandom === "You lose!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${message.options.getString("choice").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Red`)] })
+            } else if (botrpsrandom === "Match!") {
+                message.channel.send({ embeds: [new EmbedBuilder().setDescription(`You choose: ${message.options.getString("choice").trim()}.\nI choose: ${botrpsanswer}.\n\n${botrpsrandom}`).setColor(`Blue`)] })
+            }
+        }
+    }
+}
