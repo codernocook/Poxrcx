9d35fb06a53097cf0c7135618c02c2771a1bb6c2
codernocook <90613458+codernocook@users.noreply.github.com>
2022-12-31 08:02:05 +0000

    Faster translate respond!
    Yes it faster than your normal translate speed!

--- commandmodule/translate.js
+++ commandmodule/translate.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const translate = require("translate-google")
+const translate = require("@iamtraction/google-translate")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,26 +12,26 @@ module.exports = {
             option.setName("to").setDescription("The language you want to translate to").setRequired(true)
         )
         .addStringOption(option =>
-            option.setName("word").setDescription("The word you want to translate").setRequired(true)
+            option.setName("words").setDescription("The word you want to translate").setRequired(true)
         ),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
         if (typeofcommand === "message") {
             const fromlanguage = argument[0].toLowerCase();
             const tolanguage = argument[1].toLowerCase();
-            const word = argument.slice(2).join(" ");
+            const words = argument.slice(2).join(" ");
 
-            translate(word, {from: fromlanguage, to: tolanguage}).then(async (res) => {
-                await message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Translate (${fromlanguage}): ${word}\nTo (${tolanguage}): ${res}`).setColor(`Blue`)] })
+            translate(words, {from: fromlanguage, to: tolanguage}).then(async (res) => {
+                await message.channel.send({ embeds: [new EmbedBuilder().setDescription(`Translate (${fromlanguage}): ${words}\nTo (${tolanguage}): ${res.text}`).setColor(`Blue`)] })
             }).catch(err => {
                 message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> An error has occured`).setColor(`Red`)] })
             })
         } else if (typeofcommand === "interaction"){
             const fromlanguage = message.options.getString("from").toLowerCase();
             const tolanguage = message.options.getString("to").toLowerCase();
-            const word = message.options.getString("word");
+            const words = message.options.getString("words");
 
-            translate(word, {from: fromlanguage, to: tolanguage}).then(async (res) => {
-                await message.reply({ embeds: [new EmbedBuilder().setDescription(`Translate (${fromlanguage}): ${word}\nTo (${tolanguage}): ${res}`).setColor(`Blue`)] })
+            translate(words, {from: fromlanguage, to: tolanguage}).then(async (res) => {
+                await message.reply({ embeds: [new EmbedBuilder().setDescription(`Translate (${fromlanguage}): ${words}\nTo (${tolanguage}): ${res.text}`).setColor(`Blue`)] })
             }).catch(err => {
                 message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> An error has occured`).setColor(`Red`)] })
             })
