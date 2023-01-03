ffb6b16d4fcdcdcedf13186dd80dc2daf55b70aa
codernocook <90613458+codernocook@users.noreply.github.com>
2023-01-03 10:01:42 +0000

    Added Afk user check

--- commandmodule/afk.js
+++ commandmodule/afk.js
@@ -31,23 +31,28 @@ module.exports = {
             if (afk.has(message.author.id)) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You already use this command in another server.`).setColor(`Red`)] })
             const subcommand = argument[0]
             if (subcommand === "set") {
-                let reason = argument.join(" ");
-                afk.set(message.author.id, {
+                let user = message.mentions.members.first() || message.guild.members.cache.get(argument[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === argument.slice(0).join(" ") || x.user.username === argument[0]);
+                if (!user) user = message.author
+                if (afk.has(user.id)) return message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You already use this command in another server.`).setColor(`Red`)] })
+                let reason = argument.slice(1).join(" ");
+                afk.set(user.id, {
                     [1]: Date.now(),
                     [2]: reason,
                     [3]: message.guild,
-                    [4]: message.author.username
+                    [4]: user.username
                 })
+    
+                // Checking Position when change name
                 /* disabled the module cuz it lag
-                if (message.guild.members.me.roles.highest.permissions > message.guild.members.cache.find(user => message.author.id === user.id).roles.highest.permissions) {
-                    message.guild.members.cache.find(user => message.author.id === user.id).setNickname(`[AFK] ${message.author.username}`)
+                if (message.guild.members.me.roles.highest.permissions > message.guild.members.cache.find(user => message.user.id === user.id).roles.highest.permissions) {
+                    message.guild.members.cache.find(user => message.user.id === user.id).setNickname(`[AFK] ${message.user.username}`)
                 }
                 */
     
                 if (reason) {
-                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.author.id}> You are now afk for \`${reason}\`.`).setColor(`Green`)] })
+                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${user.id}> You are now afk for \`${reason}\`.`).setColor(`Green`)] })
                 } else {
-                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.author.id}> You are now afk.`).setColor(`Green`)] })
+                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${user.id}> You are now afk.`).setColor(`Green`)] })
                 }
             } else if (subcommand === "remove") {
                 let user = argument[1]
@@ -74,13 +79,15 @@ module.exports = {
             }
         } else if (typeofcommand === "interaction"){
             if (message.options.getSubcommand() === "set") {
-                if (afk.has(message.user.id)) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You already use this command in another server.`).setColor(`Red`)] })
+                let user = message.options.getMentionable("user")
+                if (!user) user = message.user
+                if (afk.has(user.id)) return message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> You already use this command in another server.`).setColor(`Red`)] })
                 let reason = message.options.getString("message")
-                afk.set(message.user.id, {
+                afk.set(user.id, {
                     [1]: Date.now(),
                     [2]: reason,
                     [3]: message.guild,
-                    [4]: message.user.username
+                    [4]: user.username
                 })
     
                 // Checking Position when change name
@@ -91,9 +98,9 @@ module.exports = {
                 */
     
                 if (reason) {
-                    message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}> You are now afk for \`${reason}\`.`).setColor(`Green`)] })
+                    message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${user.id}> You are now afk for \`${reason}\`.`).setColor(`Green`)] })
                 } else {
-                    message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${message.user.id}> You are now afk.`).setColor(`Green`)] })
+                    message.reply({ embeds: [new EmbedBuilder().setDescription(`<:PoxSuccess:1027083813123268618> <@${user.id}> You are now afk.`).setColor(`Green`)] })
                 }
             } else if (message.options.getSubcommand() === "remove") {
                 let user = message.options.getString("user")
