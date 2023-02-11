16573bb785326ba8faedf2ebca82a85d27ad4232
codernocook <90613458+codernocook@users.noreply.github.com>
2023-02-11 05:24:56 +0000

    New db!

---
+++ dbfetch.js
@@ -0,0 +1,53 @@
+require('dotenv').config({path: "./settings.env"}); // load the env
+const db = toString(process.env.db);
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = {
+    set(key, value) {
+        if (db) {
+            fetch(process.env.db, { method: "POST", body: { "key": key, "value": value }, headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                if (json) {
+                    if (json["status"] === false) return false;
+                    if (json ["status"] === true) return json;
+                }
+            })
+        }
+    },
+    has(key) {
+        if (db) {
+            fetch(process.env.db, { method: "GET", body: { "key": key, "value": value }, headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                if (json) {
+                    if (json["status"] === false) return false;
+
+                    if (json ["status"] === true) {
+                        if (json["data"] && json["data"][key]) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            })
+        }
+    },
+    get(key, value) {
+        if (db) {
+            fetch(process.env.db, { method: "GET", body: { "key": key, "value": value }, headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                if (json) {
+                    if (json["status"] === false) return false;
+                    if (json ["status"] === true) return json["data"];
+                }
+            })
+        }
+    },
+    delete(key) {
+        if (db) {
+            fetch(process.env.db, { method: "DELETE", body: { "key": key }, headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                if (json) {
+                    if (json["status"] === false) return false;
+                    if (json ["status"] === true) return json;
+                }
+            })
+        }
+    }
+}
