4c561fc26830e44706e107041afbb2251d54ac4a
codernocook <90613458+codernocook@users.noreply.github.com>
2023-03-16 07:40:11 +0000

    Improve chat and make a rate limit check

--- commandmodule/chat.js
+++ commandmodule/chat.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const youchatwrapper = require("@codernocook/youchatwrapper");
+let rate_limit = "😔 Due to high demand, I'm experiencing issues briefly. Please try again later or use the All tab to get an answer in the meantime."
 youchatwrapper.apiKey = ""
 
 module.exports = {
@@ -7,19 +8,24 @@ module.exports = {
 		.setName("chat")
 		.setDescription("Chat with YouChat (you.com), not ChatGPT (not many country support)")
         .addStringOption(option =>
-            option.setName("message").setDescription("the message to chat with AI").setRequired(true)
+            option.setName("prompt").setDescription("the message to chat with AI").setRequired(true)
         ),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
         if (typeofcommand === "message") {
             let chatmessageget = argument.slice(0).join(" ")
             if (!chatmessageget) return
             message.channel.send({ embeds: [new EmbedBuilder().setDescription("Please wait, getting answer ...").setColor(`Green`)] }).then(currentMessage => {
+                if (!currentMessage) return;
                 try {
                     youchatwrapper.chat(chatmessageget, function(callback) {
-                        currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`**Question**: ${chatmessageget}\n\n**GPT-3**: ${callback}`).setColor(`Green`)] }).catch(e => console.log(e));
+                        if (callback !== rate_limit) {
+                            currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`**Prompt**: ${chatmessageget}\n\n**GPT-3**: ${callback}`).setColor(`Green`)] }).catch(e => console.log(e));
+                        } else {
+                            currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Please try again in few seconds.`).setColor(`Green`)] }).catch(e => console.log(e));
+                        }
                     })
                 } catch {
-                    currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Can't get the answer, please try again.`).setColor(`Green`)] }).catch(e => console.log(e));
+                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Can't get the answer, please try again.`).setColor(`Green`)] }).catch(e => console.log(e));
                 }
             })
         } else if (typeofcommand === "interaction"){
@@ -28,10 +34,14 @@ module.exports = {
             message.reply({ embeds: [new EmbedBuilder().setDescription("Please wait, getting answer ...").setColor(`Green`)] }).then(currentMessage => {
                 try {
                     youchatwrapper.chat(chatmessageget, function(callback) {
-                        currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`Question: ${chatmessageget}\n\nGPT-3: ${callback}`).setColor(`Green`)] }).catch(e => console.log(e));
+                        if (callback !== rate_limit) {
+                            currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`**Prompt**: ${chatmessageget}\n\n**GPT-3**: ${callback}`).setColor(`Green`)] }).catch(e => console.log(e));
+                        } else {
+                            currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Please try again in few seconds.`).setColor(`Green`)] }).catch(e => console.log(e));
+                        }
                     })
                 } catch {
-                    currentMessage.edit({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Can't get the answer, please try again.`).setColor(`Green`)] }).catch(e => console.log(e));
+                    message.channel.send({ embeds: [new EmbedBuilder().setDescription(`<:PoxError:1025977546019450972> Can't get the answer, please try again.`).setColor(`Green`)] }).catch(e => console.log(e));
                 }
             })
         }
