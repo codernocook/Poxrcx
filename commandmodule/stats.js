4587ab67a7007f1492f6b4863f8f3a61ea2f55dd
codernocook <90613458+codernocook@users.noreply.github.com>
2023-05-24 17:23:50 +0100

    Make command more useful

--- commandmodule/stats.js
+++ commandmodule/stats.js
@@ -6,9 +6,23 @@ module.exports = {
 		.setDescription("Show the server stat."),
     execute(argument, message, EmbedBuilder, client, typeofcommand) {
         if (typeofcommand === "message") {
-            message.channel.send({ embeds: [new EmbedBuilder().setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMembers: ${message.guild.memberCount}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] })
+            function banner_url() {
+                if (message.guild.bannerURL() !== null) {
+                    return `**[Image](${message.guild.bannerURL()})**`;
+                } else if (message.guild.bannerURL() === null) {
+                    return "No";
+                }
+            }
+            message.channel.send({ embeds: [new EmbedBuilder().setThumbnail(message.guild.iconURL({ "extension": "webp", "forceStatic": false, "size": 128 })).setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMember(s): ${message.guild.memberCount}\nCreated at: ${new Date(message.guild.createdTimestamp).getDay()}/${new Date(message.guild.createdTimestamp).getMonth()}/${new Date(message.guild.createdTimestamp).getFullYear()} (dd/mm/yyyy)\nCreated at (System Time): ${message.guild.createdAt}\nBanner (url): ${banner_url()}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] });
         } else if (typeofcommand === "interaction"){
-            message.reply({ embeds: [new EmbedBuilder().setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMembers: ${message.guild.memberCount}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] })
+            function banner_url() {
+                if (message.guild.bannerURL() !== null) {
+                    return `**[Image](${message.guild.bannerURL()})**`;
+                } else if (message.guild.bannerURL() === null) {
+                    return "No";
+                }
+            }
+            message.reply({ embeds: [new EmbedBuilder().setThumbnail(message.guild.iconURL({ "extension": "webp", "forceStatic": false, "size": 128 })).setDescription(`**Server Name**: ${message.guild.name}\nOwner: <@${message.guild.ownerId}>\nMember(s): ${message.guild.memberCount}\nCreated at: ${new Date(message.guild.createdTimestamp).getDay()}/${new Date(message.guild.createdTimestamp).getMonth()}/${new Date(message.guild.createdTimestamp).getFullYear()} (dd/mm/yyyy)\nCreated at (System Time): ${message.guild.createdAt}\nBanner (url): ${banner_url()}\nNSFW Level: ${message.guild.nsfwLevel}\nVerify: ${message.guild.verified}\nDiscord Partner: ${message.guild.partnered}`).setColor(`Blue`)] });
         }
     }
 }
